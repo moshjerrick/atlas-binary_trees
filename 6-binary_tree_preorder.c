2139d0ca5a79fea3d04936c20a756aa332582440
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-15 13:22:25 -0500

    commiting a couple at once

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Move through a tree in the preorder style
+ * @tree: The root node of the tree to traverse
+ * @func: Pointer to the function to run on the node
+ **/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+		func(tree->n);
+		if (tree->left != NULL)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right != NULL)
+			binary_tree_preorder(tree->right, func);
+	}
+}
---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Find the height of the longest branch
+ * @tree: The tree to find the height of
+ * Return: left_height if left branch is bigger, right_height if right branch
+ * is bigger, 0 if tree is NULL or tree is a leaf
+ **/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	left_height = right_height = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
