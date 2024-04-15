b95ea1083c40e87019baa58c109d2c03c121e186
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-15 14:04:06 -0500

    added correct notation and hope betty is happy

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,29 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measure balance factory of binary tree
- * @tree: pointer to root of node of tree
- * 
- * Return: NULL, or return balance factor
- */
+* binary_tree_height - measure height of binary tree
+* @tree: pointer to root of node of tree
+*
+* Return: NULL, or return height
+*/
 size_t _binary_tree_height(const binary_tree_t *tree)
 {
-        int r_height, l_height;
+		int r_height, l_height;
 
-        if (!tree)
-                return (0);
-        r_height = _binary_tree_height(tree->right);
-        l_height = _binary_tree_height(tree->left);
-        return (1 + ((r_height >= l_height) ? r_height: l_height));
+		if (!tree)
+				return (0);
+		r_height = _binary_tree_height(tree->right);
+		l_height = _binary_tree_height(tree->left);
+		return (1 + ((r_height >= l_height) ? r_height: l_height));
 }
 
 
 /**
- * 
- */
+* binary_tree_balance - measure balance factory of binary tree
+* @tree: pointer to root of node of tree
+* Return: NULL else return balance factor
+*/
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-        return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
+		if (!tree)
+				return (0);
+		return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
