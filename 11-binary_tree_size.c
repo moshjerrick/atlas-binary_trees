d78888a9f5ddfaca89cea334583048341e13aac8
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-15 13:48:33 -0500

    commiting 12

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,9 +11,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 		size_t size = 0;
 
-		if (!tree)
+        if (!tree)
 				return (0);
-		if (!tree->left && !tree->right)
+        if (!tree->left && !tree->right)
 				return (1);
 
 		size += binary_tree_size(tree->left);
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Count the leaves in a tree
+ * @tree: Tree to count 
+ * Return: Number of leaves
+ **/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (!(tree->left == NULL && tree->right == NULL))
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (1);
+}
