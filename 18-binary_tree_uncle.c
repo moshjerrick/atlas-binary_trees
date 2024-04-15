b1f994b4e3545b86feb0ebb79fc3f97ded0399dd
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-15 14:44:28 -0500

    made a note 2 lines

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,7 +3,8 @@
 * binary_tree_uncle - Finds the uncle of a node in a binary tree.
 * @node: A pointer to the node to find the uncle of.
 *
-* Return: If node is NULL or has no uncle, NULL, otherwise, a pointer to the uncle node.
+* Return: If node is NULL or has no uncle, NULL, otherwise,
+    a pointer to the uncle node.
 */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
