b0ffbf788f991cfd41c0e6f8fd909ba20318eb9b
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-15 12:53:50 -0500

    added to .h file

--- binary_trees.h
+++ binary_trees.h
@@ -6,6 +6,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
