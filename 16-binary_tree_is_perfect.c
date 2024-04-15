ed5fc80377c2242a59265b59799042d881c0c93c
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-15 14:21:18 -0500

    added binary tree height

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -24,3 +24,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	return (binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right));
 }
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (1 + (left_height > right_height ? left_height : right_height));
+}
