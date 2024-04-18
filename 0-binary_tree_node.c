79c33780b4fda72aa1ce13b6559fee5e85c272a4
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-04-18 11:19:30 -0500

    betty fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_node - creates a binary tree node of struct `binary_tree_t`
- * @parent: pointer to parent of node created
- * @value: integer stored in new node
- * Return: pointer to new node
- */
+* binary_tree_node - creates a binary tree node of struct `binary_tree_t`
+* @parent: pointer to parent of node created
+* @value: integer stored in new node
+* Return: pointer to new node
+*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
